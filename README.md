b7d137210d00f1b0c755af3dcfee32499569ffa6
Liu Tong <szliutong@gmail.com>
2025-08-10 21:46:24 +0800

    Move to Jetson Orin Arm64

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Installation
 ```
+# 1. Modify pyobbecsdk version in pyproject.toml which stored in `./whl` directory.
+
+# 2. Install dependance
 conda create -n realman python=3.10
 conda activate realman
 pip install -e .
+
+# 3. Install Pytorch that satisfies your system(Referene pytorch start offical)
+pip install torch torchvision --url-index XXXX
 ```
--- scripts/run_basic.py
+++ scripts/run_basic.py
@@ -32,40 +32,40 @@ def main():
     print(colored(line, color="yellow"))
     
     # Connect Camera, Set Default and Start
-    # print(colored("Camera Initialization: ", "light_red"))
-    # gemini_controller = Gemini335Controller(
-    #     RGB_width=Gemini335Config.RGB_width,
-    #     RGB_height=Gemini335Config.RGB_height,
-    #     RGB_fps=Gemini335Config.RGB_fps,
-    #     Depth_width=Gemini335Config.Depth_width,
-    #     Depth_height=Gemini335Config.Depth_height,
-    #     Depth_fps=Gemini335Config.Depth_fps,
-    # )
-    # print(colored(line, color="yellow"))
+    print(colored("Camera Initialization: ", "light_red"))
+    gemini_controller = Gemini335Controller(
+        RGB_width=Gemini335Config.RGB_width,
+        RGB_height=Gemini335Config.RGB_height,
+        RGB_fps=Gemini335Config.RGB_fps,
+        Depth_width=Gemini335Config.Depth_width,
+        Depth_height=Gemini335Config.Depth_height,
+        Depth_fps=Gemini335Config.Depth_fps,
+    )
+    print(colored(line, color="yellow"))
 
 
     # Take photos
-    # time_stamp, RGB_image, Depth_image, Points = gemini_controller.take_photo()
-    # u, v = (400, 200)
-    # u2, v2 = (880, 200)
-    # rgb_plot = cv2.cvtColor(RGB_image, cv2.COLOR_BGR2RGB)
+    time_stamp, RGB_image, Depth_image, Points = gemini_controller.take_photo()
+    u, v = (400, 200)
+    u2, v2 = (880, 200)
+    rgb_plot = cv2.cvtColor(RGB_image, cv2.COLOR_BGR2RGB)
 
-    # print(gemini_controller.get_point_xyz_from_pointcloud(Points, u, v))
-    # print(gemini_controller.get_point_xyz_from_pointcloud(Points, u2, v2))
+    print(gemini_controller.get_point_xyz_from_pointcloud(Points, u, v))
+    print(gemini_controller.get_point_xyz_from_pointcloud(Points, u2, v2))
     
-    # plt.figure(figsize=(8, 6))
-    # plt.imshow(rgb_plot)
-    # # Mark the point (u, v) in red, size 100
-    # plt.scatter([u,u2], [v,v2], c='red', s=100, marker='o')
-    # plt.title(f"RGB Image with marked point ({u}, {v})")
-    # plt.axis('off')
-    # plt.show()
+    plt.figure(figsize=(8, 6))
+    plt.imshow(rgb_plot)
+    # Mark the point (u, v) in red, size 100
+    plt.scatter([u,u2], [v,v2], c='red', s=100, marker='o')
+    plt.title(f"RGB Image with marked point ({u}, {v})")
+    plt.axis('off')
+    plt.show()
 
     
     # Move looking at
-    eef_pos = [0.5, 0, 0]
-    look_at = [0.5, 0, -0.5]
-    arm_controller.movej_p_look_at(eef_pos, look_at, v=30)
+    # eef_pos = [0.5, 0, 0]
+    # look_at = [0.5, 0, -0.5]
+    # arm_controller.movej_p_look_at(eef_pos, look_at, v=30)
 
 
     # Disconnect
